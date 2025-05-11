2d9b111ce1ce8dcb7b121cc3eaa96f8467e5f023
Stropheum <stropheum060@gmail.com>
2025-05-11 13:51:37 -0400

    added GAS dependencies

--- Source/GasGun/GasGun.Build.cs
+++ Source/GasGun/GasGun.Build.cs
@@ -8,6 +8,16 @@ public class GasGun : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core", 
+			"CoreUObject", 
+			"Engine", 
+			"InputCore", 
+			"EnhancedInput", 
+			"GameplayAbilities", 
+			"GameplayTasks", 
+			"GameplayTags"
+		});
 	}
 }
