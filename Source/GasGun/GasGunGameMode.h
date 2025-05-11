486f7cea75f91855e4ff7e824fe39d50ad230694
Stropheum <stropheum060@gmail.com>
2025-05-11 13:06:15 -0400

    Project template imported

---
+++ Source/GasGun/GasGunGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GasGunGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AGasGunGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGasGunGameMode();
+};
+
+
+
